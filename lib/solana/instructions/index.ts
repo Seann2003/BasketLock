55c86097ad2cd870b6648006d52962e5a8937393
Sean Hoe Kai Zher <102246906+Seann2003@users.noreply.github.com>
2026-02-28 02:30:51 +0800

    feat: add instructions barrel export

---
+++ lib/solana/instructions/index.ts
@@ -0,0 +1,7 @@
+export { buildInitConfigIx } from "./init-config";
+export { buildSetConfigIx } from "./set-config";
+export { buildCreateBasketIx } from "./create-basket";
+export { buildAddTokensIx } from "./add-tokens";
+export { buildDepositMultiIx } from "./deposit-multi";
+export { buildWithdrawMultiIx } from "./withdraw-multi";
+export { buildUpdateAllowListIx } from "./update-allow-list";
